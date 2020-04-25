fedde0e04378e01b46e01320ca25b45cf023f86d
AnatoliyBr <bryushinin.aa@edu.spbstu.ru>
2020-04-25 13:07:13 +0300

    Найден оптимальный вариант анимации и изменения типа интерполяции

--- addon/script_generate_video.py
+++ addon/script_generate_video.py
@@ -1,11 +1,12 @@
 import bpy
 import json
 
-################# ВЕРСИЯ 1 #################
+################# ВЕРСИЯ 1 (upgrade) #################
+
 def interpolation_type(obj, inter_type = 'LINEAR'):
-    '''
-    Интерполяция (по стандарту - линейная)
-    '''
+    
+    #Интерполяция (по стандарту - линейная)
+    
     fc = obj.animation_data.action.fcurves
     for index in range(2,11):
         fc_index = fc[index]
@@ -20,9 +21,9 @@ def interpolation_type(obj, inter_type = 'LINEAR'):
     return
 
 def add_object(name, verts, faces, edges=[], col_name='Video_col.1'):
-    '''
-    Добавление пользовательского объекта
-    '''
+    
+    #Добавление пользовательского объекта
+    
     mesh = bpy.data.meshes.new(name)
     obj = bpy.data.objects.new(mesh.name, mesh)
     col = bpy.data.collections.get(col_name)
@@ -32,9 +33,9 @@ def add_object(name, verts, faces, edges=[], col_name='Video_col.1'):
     return obj
 
 def add_basic_object(obj, counter, col_name='Video_col.1'):
-    '''
-    Добавление базового объекта
-    '''
+    
+    #Добавление базового объекта
+    
     if obj['type'].lower() == 'cube':
         # Добавляем куб
         bpy.ops.mesh.primitive_cube_add(size=obj['size'], enter_editmode=False, location=(0, 0, 0))
@@ -80,9 +81,9 @@ def add_basic_object(obj, counter, col_name='Video_col.1'):
     return ob
 
 def animated_material(obj, color, cur_frame):
-    '''
-    Анимация цвета
-    '''
+    
+    #Анимация цвета
+    
     #color.append(1)
     if obj.name in bpy.data.materials:
         if bpy.data.materials[obj.name].node_tree.nodes['Diffuse BSDF'].inputs[0].default_value == color:
@@ -90,6 +91,11 @@ def animated_material(obj, color, cur_frame):
         else:
             bpy.data.materials[obj.name].node_tree.nodes['Diffuse BSDF'].inputs[0].default_value = color
             bpy.data.materials[obj.name].node_tree.nodes['Diffuse BSDF'].inputs[0].keyframe_insert("default_value", frame= cur_frame)
+            #upgrade
+            fc_mat = bpy.data.materials[obj.name].node_tree.animation_data.action.fcurves
+            for index in range(4):
+                fc_mat[index].keyframe_points[-1].interpolation = 'LINEAR'
+
     else:
         new_material = bpy.data.materials.new( name= obj.name)
         new_material.use_nodes = True
@@ -103,20 +109,24 @@ def animated_material(obj, color, cur_frame):
         new_material.node_tree.links.new(diffuse_node.outputs[0], material_output.inputs[0])
         #bpy.context.object.active_material = new_material
         bpy.data.objects[obj.name].data.materials.append(bpy.data.materials[obj.name])
+        #upgrade
+        fc_mat = new_material.node_tree.animation_data.action.fcurves
+        for index in range(4):
+            fc_mat[index].keyframe_points[-1].interpolation = 'LINEAR'
 
 def hide_object(obj, fr):
-    '''
-    Скрытие объекта
-    '''
+    
+    #Скрытие объекта
+    
     obj.hide_viewport = True
     obj.keyframe_insert(data_path="hide_viewport", frame=fr)
     obj.hide_render = True
     obj.keyframe_insert(data_path="hide_render", frame=fr)
 
 def uncover_object(obj, fr):
-    '''
-    Проявление объекта
-    '''
+    
+    #Проявление объекта
+    
     obj.hide_viewport = False
     obj.keyframe_insert(data_path="hide_viewport", frame=fr)
     obj.hide_render = False
@@ -168,11 +178,18 @@ def generate_video(input_filepath):
             ob.rotation_euler.x = obj['rotation']['x']
             ob.rotation_euler.y = obj['rotation']['y']
             ob.rotation_euler.z = obj['rotation']['z']
+            ob.scale = (obj['scale'][0], obj['scale'][1], obj['scale'][2])
+
             ob.keyframe_insert(data_path="location", frame=fr, index=-1)
             ob.keyframe_insert("rotation_euler", frame=fr)
-            ob.scale = (obj['scale'][0], obj['scale'][1], obj['scale'][2])
             ob.keyframe_insert("scale", frame=fr)
-            interpolation_type(ob) # Интерполяция
+
+            #upgrade
+            fc = ob.animation_data.action.fcurves
+            for index in range(2,11):
+                fc[index].keyframe_points[-1].interpolation = 'LINEAR'
+                   
+            #interpolation_type(ob) # Интерполяция
         
         set_be_now = set(be_now)
         set_be = set(be)
@@ -183,6 +200,7 @@ def generate_video(input_filepath):
                 hide_object(obj, fr)
 
     return fr
+
 
 ################# ВЕРСИЯ 2 #################
 '''
@@ -354,11 +372,11 @@ def generate_video(input_filepath):
             if obj['id'] not in be:
                 if obj['type'].lower() in basic_objects:
                     ob = add_basic_object(obj, counter, col_name=newCol.name)
-                    object_animation(ob) #Анимируем объект
+                    object_animation(ob) #Анимируем объект 
                 else:
                     ob = add_object(obj['id']+'.'+counter, obj['size']['verts'], obj['size']['faces'],
                                     edges=[], col_name=newCol.name)
-                    object_animation(ob) #Анимируем объект
+                    object_animation(ob) #Анимируем объект 
                 be.append(obj['id'])
                 hide_object(ob, 0)
    
@@ -369,7 +387,7 @@ def generate_video(input_filepath):
         for obj in time_frame['objects']:
             ob = bpy.data.objects[obj['id']+'.'+counter]
             be_now.append(obj['id'])
-
+            
             uncover_object(ob, fr)
             
             animated_material(ob, obj['color'], fr)
@@ -420,4 +438,5 @@ def generate_video(input_filepath):
                 hide_object(obj, fr)
 
     return fr
+
 '''
